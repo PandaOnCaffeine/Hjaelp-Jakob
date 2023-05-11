9191e835ed4e08787e3623197545227a7f19f4ab
StationaryCaffeine <CaptPandaa@gmail.com>
2023-05-11 18:07:58 +0200

    Add project files.

---
+++ Hjaelp Jakob/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hjaelp_Jakob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public enum MessageCarrier { Smtp, VMessage }
+
+    public class SenderAndConverter
+    {
+        public string ConvertBodyToHTML(string plainText)
+        {
+            return "" + plainText + "";
+        }
+        public void sendMessage(MessageCarrier type, Message m, bool isHTML)
+        {
+            //herinde sendes der en email ud til modtageren
+            if (type.Equals(MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+        public void sendMessageToAll(MessageCarrier type, string[] to, Message m, bool isHTML)
+        {
+            if (type.Equals(MessageCarrier.Smtp))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via Smtp
+            }
+
+            if (type.Equals(MessageCarrier.VMessage))
+            {
+                if (isHTML)
+                    m.Body = ConvertBodyToHTML(m.Body);
+                //her implementeres alt koden til at sende via VMessage
+            }
+        }
+    }
+    public class Message
+    {
+        string to, from, body, subject, cc;
+        public Message(string to, string from, string body, string subject, string cc)
+        {
+            this.to = to;
+            this.from = from;
+            this.body = body;
+            this.subject = subject;
+            this.cc = cc;
+        }
+        public string To { get => to; set => to = value; }
+        public string From { get => from; set => from = value; }
+        public string Body { get => body; set => body = value; }
+        public string Subject { get => subject; set => subject = value; }
+        public string Cc { get => cc; set => cc = value; }
+    }
+}
